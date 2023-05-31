cfefab695d18c159029f2a9f08ef4c3b822d8fd9
aditya <aditya1207khare@gmail.com>
2023-05-31 20:05:59 +0530

    Model Trained

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -11,6 +11,7 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+from src.components.model_trainer import ModelTrainerConfig, ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -54,5 +55,7 @@ if __name__ == "__main__":
     obj = DataIngestion()
     train_path,test_path = obj.initiate_data_ingestion()
     obj_dataTransformation = DataTransformation()
-    obj_dataTransformation.initiate_data_transformation(train_path, test_path)
+    train_arr,test_arr,_= obj_dataTransformation.initiate_data_transformation(train_path, test_path)
+    obj_model = ModelTrainer()
+    print(obj_model.initiate_model_trainer(train_arr,test_arr))
     
